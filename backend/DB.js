c3835a13c952ff7a504d898e27cb057784d7c343
Divyanshpatel18 <82250981+Divyanshpatel18@users.noreply.github.com>
2023-05-11 15:43:45 +0530

    Update DB.js

--- backend/DB.js
+++ backend/DB.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb+srv://admin:admin@cluster0.aobui4w.mongodb.net/inotebook"
+const mongoURI=""
+//mongo URI not public 
 const connectToMongo = async()=>{
     mongoose.connect(mongoURI).then(()=>{
         console.log("connected to Mongo successfully");
